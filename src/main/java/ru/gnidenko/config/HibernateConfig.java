c1c607207ccab3e9d2a9a355d2d5245f9f2bc465
nick-long-int <gnidenkonn@yandex.ru>
2026-01-21 02:17:43 +0300

    Добавление конфига для SessionFactory

---
+++ src/main/java/ru/gnidenko/config/HibernateConfig.java
@@ -0,0 +1,28 @@
+package ru.gnidenko.config;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ru.gnidenko.model.Block;
+import ru.gnidenko.model.Comment;
+import ru.gnidenko.model.Page;
+import ru.gnidenko.model.Tag;
+import ru.gnidenko.model.User;
+
+public class HibernateConfig {
+    public static SessionFactory getSessionFactory() {
+        return new Configuration()
+            .addAnnotatedClasses(User.class)
+            .addAnnotatedClass(Page.class)
+            .addAnnotatedClass(Block.class)
+            .addAnnotatedClass(Comment.class)
+            .addAnnotatedClass(Tag.class)
+            .setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect")
+            .setProperty("hibernate.connection.driver_class", "org.postgresql.Driver")
+            .setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5433/short-notes-db")
+            .setProperty("hibernate.connection.username", "admin")
+            .setProperty("hibernate.connection.password", "admin")
+            .setProperty("hibernate.hbm2ddl.auto", "update")
+            .setProperty("hibernate.show_sql", "true")
+            .buildSessionFactory();
+    }
+}
