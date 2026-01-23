de0dda89b034af102099ae512599cb67b013dc14
nick-long-int <gnidenkonn@yandex.ru>
2026-01-24 01:12:15 +0300

    Изменение версии elastic

--- src/main/java/ru/gnidenko/config/ElasticConfig.java
+++ src/main/java/ru/gnidenko/config/ElasticConfig.java
@@ -2,26 +2,21 @@ package ru.gnidenko.config;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
-import co.elastic.clients.transport.rest5_client.Rest5ClientTransport;
-import co.elastic.clients.transport.rest5_client.low_level.Rest5Client;
-
-import java.io.IOException;
-import java.net.URI;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
 
 public class ElasticConfig {
     public static ElasticsearchClient getElasticsearchClient() {
-        try (Rest5Client restClient = Rest5Client
-            .builder(URI.create("http://localhost:9200"))
-            .build();
-             Rest5ClientTransport transport = new Rest5ClientTransport(
-                 restClient, new JacksonJsonpMapper()
-             );
-             ElasticsearchClient elasticsearchClient = new ElasticsearchClient(transport)
-        ) {
-            return elasticsearchClient;
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+            RestClient restClient = RestClient.builder(
+                new HttpHost("localhost", 9200, "http")
+            ).build();
+            RestClientTransport transport = new RestClientTransport(
+                restClient, new JacksonJsonpMapper()
+            );
+        return new ElasticsearchClient(transport);
+
+
     }
 }
